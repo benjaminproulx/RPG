72de34ca0ca56731b5ea6bb4e222f1e45350d020
martonks <martycal@Outlook.com>
2020-05-12 20:26:52 -0400

    Added same code for other NPCs

--- rpg/Dialogue.java
+++ rpg/Dialogue.java
@@ -37,21 +37,105 @@ public class Dialogue {
                 default: System.out.println("Alright then, goodbye.");
             }
 
-        } else if (npcNumber==1){
-            System.out.println("Jack");
+        } else if(npcNumber==1){
+            System.out.println(npc.getName());
             System.out.println("\nWhat would you like to do?");
+            System.out.println("\n(1) Ask about his day");
+            System.out.println("(2) Ask if he has a key");
+            System.out.println("(0) Say Goodbye");
+            
+            input = sc.nextInt();
 
-        } else if (npcNumber==2){
-            System.out.println("Jack");
-            System.out.println("\nWhat would you like to do?");
+            switch(input){
+                case 1: System.out.println("My day has been fine, thank you.");
+                break;
 
-        } else if (npcNumber==3){
-            System.out.println("Jack");
-            System.out.println("\nWhat would you like to do?");
+                case 2: 
+                if(npc.hasKey()){
+                    player.giveKey(npc.getKey());
+                    System.out.println("Here is my key!");
+                } else {
+                    System.out.println("No I don't have a key.");
+                }
+                break;
 
-        } else if (npcNumber==4){
-            System.out.println("Jack");
+                default: System.out.println("Alright then, goodbye.");
+            }
+
+        } else if(npcNumber==2){
+            System.out.println(npc.getName());
             System.out.println("\nWhat would you like to do?");
+            System.out.println("\n(1) Ask about his day");
+            System.out.println("(2) Ask if he has a key");
+            System.out.println("(0) Say Goodbye");
+            
+            input = sc.nextInt();
+
+            switch(input){
+                case 1: System.out.println("My day has been fine, thank you.");
+                break;
+
+                case 2: 
+                if(npc.hasKey()){
+                    player.giveKey(npc.getKey());
+                    System.out.println("Here is my key!");
+                } else {
+                    System.out.println("No I don't have a key.");
+                }
+                break;
+
+                default: System.out.println("Alright then, goodbye.");
+            }
+
+        } else if(npcNumber==3){
+            System.out.println(npc.getName());
+            System.out.println("\nWhat would you like to do?");
+            System.out.println("\n(1) Ask about his day");
+            System.out.println("(2) Ask if he has a key");
+            System.out.println("(0) Say Goodbye");
+            
+            input = sc.nextInt();
+
+            switch(input){
+                case 1: System.out.println("My day has been fine, thank you.");
+                break;
+
+                case 2: 
+                if(npc.hasKey()){
+                    player.giveKey(npc.getKey());
+                    System.out.println("Here is my key!");
+                } else {
+                    System.out.println("No I don't have a key.");
+                }
+                break;
+
+                default: System.out.println("Alright then, goodbye.");
+            }
+
+        } else if(npcNumber==4){
+            System.out.println(npc.getName());
+            System.out.println("\nWhat would you like to do?");
+            System.out.println("\n(1) Ask about his day");
+            System.out.println("(2) Ask if he has a key");
+            System.out.println("(0) Say Goodbye");
+            
+            input = sc.nextInt();
+
+            switch(input){
+                case 1: System.out.println("My day has been fine, thank you.");
+                break;
+
+                case 2: 
+                if(npc.hasKey()){
+                    player.giveKey(npc.getKey());
+                    System.out.println("Here is my key!");
+                } else {
+                    System.out.println("No I don't have a key.");
+                }
+                break;
+
+                default: System.out.println("Alright then, goodbye.");
+            }
 
         }
         
